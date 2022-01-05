e6088f5cbf84acfd429f62ad53abb75ae1b8a76a
15776575640 <15776575640@163.com>
2022-01-05 22:30:20 +0800

    配置文件配置

---
+++ shop-manager/src/main/java/org/wanglei/manager/ManagerApplication.java
@@ -0,0 +1,16 @@
+package org.wanglei.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 王磊
+ * @Date: 2022/01/05/21:39
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class,args);
+    }
+}
